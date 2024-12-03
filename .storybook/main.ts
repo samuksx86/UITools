6820b14cdfec9ef06e0fcb835ba433e96655f0dd
Samuel <samuks2005@outlook.com>
2024-12-02 22:54:28 -0300

    Create initial component by example.

---
+++ .storybook/main.ts
@@ -0,0 +1,35 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  core: {
+    builder: "@storybook/builder-vite", // 👈 The builder enabled here.
+  },
+  async viteFinal(config, { configType }) {
+    // Merge custom configuration into the default config
+    const { mergeConfig } = await import("vite");
+    if (configType === "DEVELOPMENT") {
+      // Your development configuration goes here
+    }
+    if (configType === "PRODUCTION") {
+      // Your production configuration goes here.
+    }
+    return mergeConfig(config, {
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: ["storybook-dark-mode"],
+      },
+    });
+  },
+};
+export default config;
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,7 @@
-export const Button = () => {
-  return (
-    <div>
-      <span className="text-3xl font-bold underline">Button</span>
-    </div>
-  );
+import { ComponentProps } from "react";
+
+type ButtonProps = ComponentProps<"button">;
+
+export const Button = ({ ...props }: ButtonProps) => {
+  return <button className="text-3xl font-bold" {...props} />;
 };
